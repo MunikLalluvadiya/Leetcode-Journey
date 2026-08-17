ad9e99dbe6dbd2b45b65623b73abebb7c8fd701f
Munik Lalluvadiya <munikgajjar001@gmail.com>
2026-08-17 18:35:44 +0530

    Time: 0 ms (100%), Space: 19.9 MB (43.46%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,10 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target > max(nums):
+            return len(nums)
+        else:
+            for i in range(len(nums)):
+                if nums[i] == target:
+                    return i
+                elif nums[i] > target:
+                    return i
