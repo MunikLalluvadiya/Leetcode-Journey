872a0c5c99f2938b7b2db0aa02a920c677553c58
Munik Lalluvadiya <munikgajjar001@gmail.com>
2026-09-14 17:01:06 +0530

    Time: 31 ms (72.12%), Space: 21.8 MB (56.7%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -2,16 +2,14 @@ class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         count = 0
         curr_sum = 0
-        sum_freq = {0: 1}  # important: empty prefix sum occurs once
+        sum_freq = {0: 1}  
 
         for num in nums:
             curr_sum += num
             
-            # if (curr_sum - k) exists in map, those subarrays sum to k
             if (curr_sum - k) in sum_freq:
                 count += sum_freq[curr_sum - k]
             
-            # record current running sum
             sum_freq[curr_sum] = sum_freq.get(curr_sum, 0) + 1
 
         return count
